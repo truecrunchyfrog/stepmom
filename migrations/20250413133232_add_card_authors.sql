6009c26b7767a0b3d65795343f5460e92a7e637b
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-04-13 21:09:20 +0200

    Add trade card SQL scripts to migrations

---
+++ migrations/20250413133232_add_card_authors.sql
@@ -0,0 +1,6 @@
+INSERT INTO trade_card_authors VALUES
+  (1, "shoe", 307999381213151243),
+  (2, "anivya", 277708587554308096),
+  (3, "amelia", 645256353966981120),
+  (4, "archetim", 195258903368302592),
+  (5, "crunchyfrog", 482459199213928459)

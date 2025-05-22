6f808ceccacee91a4509e01bfed22cb315beabda
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-05-22 08:21:31 +0200

    chore(flake): add pkg-config and fontconfig

--- flake.nix
+++ flake.nix
@@ -14,7 +14,9 @@
       {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer sqlx-cli discordchatexporter-cli ];
+          buildInputs = [
+            cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer sqlx-cli discordchatexporter-cli pkg-config fontconfig
+          ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       }
